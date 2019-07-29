2acbe604c5a13304a7a95743e89cb9c83e2289ee
Camacho <c_olavo@hotmail.com>
2019-07-29 01:26:27 -0300

    Bootstrap and fonts

--- app/Views/minhaview.php
+++ app/Views/minhaview.php
@@ -1,6 +1,17 @@
 <?php 
-   // minhaview
-   echo "Teste";
+
+   //Bootstrap path
+   $bootstrap = base_url('assets/css/bootstrap.min.css');
+   $fontawesome = base_url('assets/css/all.css');
+ 
 ?>
 
-<h3>Projeto Geral</h3>
+<link rel="stylesheet" type="text/css" href="<?php echo $bootstrap; ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo $fontawesome; ?>">
+
+
+<h3><i class="fa fa-user mr-3"></i>Projeto Geral</h3>
+<br>
+
+<i class="fas fa-user"></i> <!-- uses solid style -->
+<i class="far fa-user"></i> <!-- uses regular style -->  
